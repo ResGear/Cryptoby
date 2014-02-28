e048909f53af4e93a5322d84f6727d1d2e590889
Toby <Toby@10.250.83.157>
2014-02-28 15:27:13 +0100

    Create UiConsole Class

---
+++ src/main/java/ch/zhaw/cryptoby/ui/imp/CryptobyConsole.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ch.zhaw.cryptoby.ui.imp;
+
+/**
+ *
+ * @author Toby
+ */
+public class CryptobyConsole {
+    
+}
