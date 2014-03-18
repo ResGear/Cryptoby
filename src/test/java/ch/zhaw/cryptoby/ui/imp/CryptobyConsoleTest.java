ff3429e6bbfcd361a2c705c9712010093575139a
Reesli <tobiasrees@gmail.com>
2014-03-18 22:04:05 +0100

    Update

--- src/test/java/ch/zhaw/cryptoby/ui/imp/CryptobyConsoleTest.java
+++ src/test/java/ch/zhaw/cryptoby/ui/imp/CryptobyConsoleTest.java
@@ -9,7 +9,6 @@ package ch.zhaw.cryptoby.ui.imp;
 import ch.zhaw.cryptoby.client.CryptobyClient;
 import org.junit.After;
 import org.junit.AfterClass;
-import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
