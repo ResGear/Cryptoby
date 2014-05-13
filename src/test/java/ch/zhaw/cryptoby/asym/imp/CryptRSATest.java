2692dca146772719e75f2d8c3effcd906ace365b
Reesli <tobiasrees@gmail.com>
2014-05-13 23:57:26 +0200

    Extended Tests

--- src/test/java/ch/zhaw/cryptoby/asym/imp/CryptRSATest.java
+++ src/test/java/ch/zhaw/cryptoby/asym/imp/CryptRSATest.java
@@ -14,14 +14,15 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 package ch.zhaw.cryptoby.asym.imp;
 
 import ch.zhaw.cryptoby.client.CryptobyClient;
 import ch.zhaw.cryptoby.core.CryptobyCore;
+import ch.zhaw.cryptoby.helper.CryptobyHelper;
 import ch.zhaw.cryptoby.keygen.imp.KeyGenRSA;
 import java.math.BigInteger;
 import java.util.Arrays;
+import java.util.Random;
 import org.junit.After;
 import org.junit.AfterClass;
 import static org.junit.Assert.*;
@@ -58,22 +59,61 @@ public class CryptRSATest {
      * Test of encrypt method, of class CryptRSA.
      */
     @Test
-    public void testRSACrypt1024() {
+    public void testRSACrypt1024_oneBlock() {
         System.out.println("RSACrypt1024");
         int keySize = 1024;
         CryptobyClient client = new CryptobyClient();
         CryptobyCore core = new CryptobyCore(client);
         KeyGenRSA generator = new KeyGenRSA(core);
         generator.initGenerator(keySize);
-        byte[] plainInput = "Text to Test for Testing from Tester by Testcase".getBytes();
-        String publicKeyString = generator.getPublicKey();
-        byte[] publicKey = new BigInteger(publicKeyString, Character.MAX_RADIX).toByteArray();
-        String privateKeyString = generator.getPrivateKey();
-        byte[] privateKey = new BigInteger(privateKeyString, Character.MAX_RADIX).toByteArray();
+        String smallString = "Text to Test for Testing from Tester by Testcase."
+                + "Text to Test for Testing from Tester by Testcase.Text to Test";
+        byte[] plainInput = smallString.getBytes();
+        byte[] publicKey = generator.getPublicKeyByte();
+        byte[] privateKey = generator.getPrivateKeyByte();
         CryptRSA rsa = new CryptRSA();
         byte[] expResult = plainInput;
         byte[] result = rsa.encrypt(plainInput, publicKey);
         result = rsa.decrypt(result, privateKey);
+        String reString = new String(result);
+        System.out.println(reString);
+        assertArrayEquals(expResult, result);
+    }
+
+    @Test
+    public void testRSACrypt1024_manyBlock() {
+        System.out.println("RSACrypt1024");
+        int keySize = 1024;
+        CryptobyClient client = new CryptobyClient();
+        CryptobyCore core = new CryptobyCore(client);
+        KeyGenRSA generator = new KeyGenRSA(core);
+        generator.initGenerator(keySize);
+        String longString = "Warmly little before cousin sussex entire men set. "
+                + "Blessing it ladyship on sensible judgment settling outweigh. "
+                + "Worse linen an of civil jokes leave offer. Parties all clothes"
+                + " removal cheered calling prudent her. And residence for met "
+                + "the estimable disposing. Mean if he they been no hold mr. Is "
+                + "at much do made took held help. Latter person am secure of "
+                + "estate genius at.Six started far placing saw respect females "
+                + "old. Civilly why how end viewing attempt related enquire visitor."
+                + " Man particular insensible celebrated conviction stimulated "
+                + "principles day. Sure fail or in said west. Right my front it "
+                + "wound cause fully am sorry if. She jointure goodness interest "
+                + "debating did outweigh. Is time from them full my gone in went."
+                + " Of no introduced am literature excellence mr stimulated "
+                + "contrasted increasing. Age sold some full like rich new. "
+                + "Amounted repeated as believed in confined juvenile.Started his"
+                + " hearted any civilly. So me by marianne admitted speaking. "
+                + "Men bred fine call ask. Cease one miles truth day above seven. "
+                + "Suspicion sportsmen provision suffering mrs saw engrossed something. "
+                + "Snug soon he on plan in be dine some.";
+        byte[] plainInput = longString.getBytes();
+        byte[] publicKey = generator.getPublicKeyByte();
+        byte[] privateKey = generator.getPrivateKeyByte();
+        CryptRSA rsa = new CryptRSA();
+        byte[] expResult = plainInput;
+        byte[] enc = rsa.encrypt(plainInput, publicKey);
+        byte[] result = rsa.decrypt(enc, privateKey);
         String reString = new String(result);
         System.out.println(reString);
         assertArrayEquals(expResult, result);
@@ -89,37 +129,35 @@ public class CryptRSATest {
         generator.initGenerator(keySize);
         byte[] plainInput = "Text to Test for Testing from Tester by Testcase".getBytes();
         String publicKeyString = generator.getPublicKey();
-        byte[] publicKey = new BigInteger(publicKeyString, Character.MAX_RADIX).toByteArray();
+        byte[] publicKey = CryptobyHelper.hexStringToBytes(publicKeyString);
         generator.initGenerator(keySize);
         String privateKeyString = generator.getPrivateKey();
-        byte[] privateKey = new BigInteger(privateKeyString, Character.MAX_RADIX).toByteArray();
+        byte[] privateKey = CryptobyHelper.hexStringToBytes(privateKeyString);
         CryptRSA rsa = new CryptRSA();
         byte[] expResult = plainInput;
         byte[] result = rsa.encrypt(plainInput, publicKey);
         result = rsa.decrypt(result, privateKey);
-        
+
         assertFalse(Arrays.equals(expResult, result));
     }
 
-    @Test
-    public void testRSACrypt2048() {
-        System.out.println("RSACrypt2048");
-        int keySize = 2048;
-        CryptobyClient client = new CryptobyClient();
-        CryptobyCore core = new CryptobyCore(client);
-        KeyGenRSA generator = new KeyGenRSA(core);
-        generator.initGenerator(keySize);
-        byte[] plainInput = "Text to Test for Testing from Tester by Testcase".getBytes();
-        String publicKeyString = generator.getPublicKey();
-        byte[] publicKey = new BigInteger(publicKeyString, Character.MAX_RADIX).toByteArray();
-        String privateKeyString = generator.getPrivateKey();
-        byte[] privateKey = new BigInteger(privateKeyString, Character.MAX_RADIX).toByteArray();
-        CryptRSA rsa = new CryptRSA();
-        byte[] expResult = plainInput;
-        byte[] result = rsa.encrypt(plainInput, publicKey);
-        result = rsa.decrypt(result, privateKey);
-        assertArrayEquals(expResult, result);
-    }
-
-
+//    @Test
+//    public void testRSACrypt2048() {
+//        System.out.println("RSACrypt2048");
+//        int keySize = 2048;
+//        CryptobyClient client = new CryptobyClient();
+//        CryptobyCore core = new CryptobyCore(client);
+//        KeyGenRSA generator = new KeyGenRSA(core);
+//        generator.initGenerator(keySize);
+//        byte[] plainInput = "Text to Test for Testing from Tester by Testcase".getBytes();
+//        String publicKeyString = generator.getPublicKey();
+//        byte[] publicKey = CryptobyHelper.hexStringToBytes(publicKeyString);
+//        String privateKeyString = generator.getPrivateKey();
+//        byte[] privateKey = CryptobyHelper.hexStringToBytes(privateKeyString);
+//        CryptRSA rsa = new CryptRSA();
+//        byte[] expResult = plainInput;
+//        byte[] result = rsa.encrypt(plainInput, publicKey);
+//        result = rsa.decrypt(result, privateKey);
+//        assertArrayEquals(expResult, result);
+//    }
 }
