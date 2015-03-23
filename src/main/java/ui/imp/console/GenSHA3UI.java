586f8b815a1fb6c8d2131364fd867f23c58bc01c
Toby <Toby@Toby-PC.home>
2015-03-23 13:46:44 +0100

    File Error jump back was false. Corrected.

--- src/main/java/ui/imp/console/GenSHA3UI.java
+++ src/main/java/ui/imp/console/GenSHA3UI.java
@@ -198,7 +198,7 @@ public class GenSHA3UI {
         System.out.println("Enter Path to saving Private Key(Type '" + quit + "' to Escape):");
         scanner.useDelimiter("\n");
         if (scanner.hasNext(quit)) {
-            RsaUI.rsaCrypterFile(console);
+            GenSHA3UI.genSHA3KeyFile(console);
         }
         keyPath = scanner.next();
 
@@ -219,7 +219,7 @@ public class GenSHA3UI {
             CryptobyFileManager.putKeyToFile(keyPath, key);
         } catch (IOException ex) {
             CryptobyHelper.printIOExp();
-            RsaUI.rsaCrypterFile(console);
+            GenSHA3UI.genSHA3KeyFile(console);
         }
         System.out.println("\nAES Key File saved to this Path:");
         System.out.println(keyPath);
