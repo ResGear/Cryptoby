78a9400d3798bb12bb538b6d8c1d6665e9108849
Reesli <tobiasrees@gmail.com>
2015-08-11 15:47:41 +0200

    Changed test data to smaller arrays

--- src/perftest/encrypts/TestRSA1024.java
+++ src/perftest/encrypts/TestRSA1024.java
@@ -2,7 +2,6 @@ package encrypts;
 
 import static org.junit.Assert.assertArrayEquals;
 
-import java.math.BigInteger;
 import org.junit.*;
 
 import asym.imp.CryptRSA;
@@ -17,9 +16,9 @@ public class TestRSA1024 {
 	byte[] preData;
 	byte[] modData;
 	byte[] resData;
+	int kb10;
+	int kb50;
 	int kb100;
-	int kb500;
-	int mb1;
 	int keySize = 1024;
 	byte[] pubKey;
 	byte[] privKey;
@@ -33,29 +32,29 @@ public class TestRSA1024 {
 	@Before
 	public void setUp()
 	{ 
-		kb100 = 1024 * 100;
-		kb500 = kb100 * 5;
-		mb1 = kb100 * 10;
+		kb10 = 1024 * 10;
+		kb50 = kb10 * 5;
+		kb100 = kb10 * 10;
         client = new CryptobyClient();
         core = new CryptobyCore(client);
         generator = new KeyGenRSA(core);
         rsa = new CryptRSA();
 	}
 
-	public void testRSA1024Enc() {
+	public void testRSAEnc() {
 		modData = rsa.encrypt(preData, pubKey);
 	}
 	
-	public void testRSA1024Dec() {
+	public void testRSADec() {
 		resData = rsa.decrypt(modData, privKey);
 	}
 
 
 	@Test
 	public void testRun(){
-		int rounds = 1;
+		int rounds = 50;
 		
-		System.out.println("Test Performance RSA Encryption/Decryption");
+		System.out.println("Test Performance RSA Enc/Dec");
 		System.out.println("Rounds: "+rounds);
 		System.out.println("Cycle per Round: "+cycles);
 		
@@ -63,30 +62,32 @@ public class TestRSA1024 {
 		pubKey = generator.getPublicKeyByte();
 		privKey = generator.getPrivateKeyByte();
 		
+		preData = new byte[kb10];
+		System.out.println ("\nEncrypt RSA 1024 Bit 10KB");
+		PerfMeter.run(this, rounds, "testRSAEnc");
+		System.out.println ("\nDecrypt RSA 1024 Bit 10KB");
+		PerfMeter.run(this, rounds, "testRSADec");
+		assertArrayEquals(preData, resData);
+		
+		modData = null;
+		resData = null;
+		preData = new byte[kb50];
+		System.out.println ("\nEncrypt RSA 1024 Bit 50KB");
+		PerfMeter.run(this, rounds, "testRSAEnc");
+		System.out.println ("\nDecrypt RSA 1024 Bit 50KB");
+		PerfMeter.run(this, rounds, "testRSADec");
+		assertArrayEquals(preData, resData);
+		
+		modData = null;
+		resData = null;
 		preData = new byte[kb100];
 		System.out.println ("\nEncrypt RSA 1024 Bit 100KB");
-		PerfMeter.run(this, rounds, "testRSA1024Enc");
+		PerfMeter.run(this, rounds, "testRSAEnc");
 		System.out.println ("\nDecrypt RSA 1024 Bit 100KB");
-		PerfMeter.run(this, rounds, "testRSA1024Dec");
-		assertArrayEquals(preData, resData);
-		
-		modData = null;
-		resData = null;
-		preData = new byte[kb500];
-		System.out.println ("\nEncrypt RSA 1024 Bit 500KB");
-		PerfMeter.run(this, rounds, "testRSA1024Enc");
-		System.out.println ("\nDecrypt RSA 1024 Bit 500KB");
-		PerfMeter.run(this, rounds, "testRSA1024Dec");
-		assertArrayEquals(preData, resData);
-		
-		modData = null;
-		resData = null;
-		preData = new byte[mb1];
-		System.out.println ("\nEncrypt RSA 1024 Bit 1MB");
-		PerfMeter.run(this, rounds, "testRSA1024Enc");
-		System.out.println ("\nDecrypt RSA 1024 Bit 1MB");
-		PerfMeter.run(this, rounds, "testRSA1024Dec");
+		PerfMeter.run(this, rounds, "testRSADec");
 		assertArrayEquals(preData, resData);
 	}
+	
+	
 
 }
